5a1a79be838736895727fc2edadd441f8443d7cd
Parth-bathe0142 <parth.bathe0142@gmail.com>
2026-02-04 22:27:58 +0530

    removed nested .git folder

---
+++ server/src/main.rs
@@ -0,0 +1,15 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async  fn main() {
+    let app = Router::new()
+        .route("/test", get(test));
+    
+    let port = 3000;
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port)).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn test() -> &'static str {
+    "Hello World!"
+}
