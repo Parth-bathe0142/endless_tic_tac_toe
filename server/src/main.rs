9f8414d7c7bf01a334a9e9427fbdf72c707e03aa
Parth-bathe0142 <parth.bathe0142@gmail.com>
2026-02-06 21:59:16 +0530

    added MySql with sqlx to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,41 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, extract::State, routing::get};
+use serde::Serialize;
+use sqlx::{MySqlPool, prelude::*};
+use tokio::net::TcpListener;
+
+#[derive(FromRow, Serialize)]
+struct User {
+    id: i32,
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/test", get(test));
-
-    let port = 3000;
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
+    let pool = MySqlPool::connect("mysql://root:admin@localhost:3306/tic_tac_toe")
         .await
         .unwrap();
-    
+
+    let app = Router::new()
+        .route("/test", get(test))
+        .route("/users", get(test_sql))
+        .with_state(pool);
+
+    let port = 3000;
+    let listener = TcpListener::bind(("0.0.0.0", port)).await.unwrap();
+
     println!("Listening at http://localhost:{port}");
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn test() -> &'static str {
     "Hello World!"
+}
+
+async fn test_sql(State(pool): State<MySqlPool>) -> Json<Vec<User>> {
+    let users = sqlx::query_as::<_, User>("select id, `name` from Users")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    Json(users)
 }
