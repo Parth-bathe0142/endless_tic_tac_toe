bccce0dab5dd855de949f8c352f01aa775498586
Parth-bathe0142 <parth.bathe0142@gmail.com>
2026-02-21 19:44:05 +0530

    added tailwind

---
+++ application/src/components/Board.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "../app.css";
+    import Square from "./Square.svelte";
+    const board = $state([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    const handleClick = (index: number) => {
+        board[index] += 1;
+    };
+</script>
+
+<div
+    class="grid grid-cols-3 grid-rows-3 bg-gray-900 w-fit gap-1 p-1 outline-2 outline-gray-700 rounded-xs"
+>
+    {#each board as value, index}
+        <Square {handleClick} {value} {index} />
+    {/each}
+</div>
---
+++ application/src/components/Square.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "../app.css";
+    const { value, handleClick, index } = $props();
+</script>
+
+<div class="h-16 w-16 outline-2 outline-gray-700 text-white">
+    <button
+        onclick={() => handleClick(index)}
+        class="h-full w-full flex items-center justify-center">{value}</button
+    >
+</div>
