f8a1787929830ad91cc96f4c03e2079b51a0e370
Parth-Bathe0142 <parth.bathe0142@gmail.com>
2026-04-12 22:40:45 +0530

    Major change: rearranged the project to separate frontend from tauri for reuse, switched to sqlite on the server for compatibility with wasmer

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,17 @@
-use axum::{Json, Router, extract::State, routing::get};
-use serde::Serialize;
-use sqlx::{MySqlPool, prelude::*};
+use axum::{Router, routing::get};
 use tokio::net::TcpListener;
+use tower_http::services::{ServeDir, ServeFile};
 
-#[derive(FromRow, Serialize)]
-struct User {
-    id: i32,
-    name: String,
-}
+
 
 #[tokio::main]
 async fn main() {
-    let pool = MySqlPool::connect("mysql://root:admin@localhost:3306/tic_tac_toe")
-        .await
-        .unwrap();
+    let pool = sqlx::SqlitePool::connect("sqlite:./database.db?mode=rwc").await.unwrap();
 
     let app = Router::new()
+        .nest_service("/", ServeDir::new("../frontend/build")
+            .not_found_service(ServeFile::new("../frontend/build/index.html")))
         .route("/test", get(test))
-        .route("/users", get(test_sql))
         .with_state(pool);
 
     let port = 3000;
@@ -29,13 +23,4 @@ async fn main() {
 
 async fn test() -> &'static str {
     "Hello World!"
-}
-
-async fn test_sql(State(pool): State<MySqlPool>) -> Json<Vec<User>> {
-    let users = sqlx::query_as::<_, User>("select id, `name` from Users")
-        .fetch_all(&pool)
-        .await
-        .unwrap();
-
-    Json(users)
 }
